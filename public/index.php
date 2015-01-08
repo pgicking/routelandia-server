e988fa1499967713745037d9e7ff2d794e1132d7
Josh Proehl <josh@daedalusdreams.com>
2015-01-08 15:22:02 -0800

    Add comments and resources for explorer

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ require_once '../vendor/restler.php';
 
 use Luracast\Restler\Restler;
 
-$r = new Restler();
+$r = new Restler();  // Pass "true" to this to put Restler into production mode
 $r->addAPIClass('Highway');
+$r->addAPIClass('Resources'); //this creates resources.json at API Root
 $r->handle();
 
