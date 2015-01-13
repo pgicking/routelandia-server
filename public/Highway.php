bd8b7eb385fe737a31217f5f27b5ec67bf8c7252
Josh Proehl <josh@daedalusdreams.com>
2015-01-12 18:32:37 -0800

    Correctly fetch single highway, docs update

--- public/Highway.php
+++ public/Highway.php
@@ -22,19 +22,22 @@ class Highway {
    *
    * Returns the highway with the passed in ID with no processing or alteration.
    *
-   * @param int $id The ID of the highway you'd like to view.
+   * @access public
+   * @param int $id The database ID of the highway you'd like to view.
    * @return Highway The highway requested.
    */
   function get($id) {
-    return DB::instance()->highways->fetch($id);
+    return DB::instance()->highways[$id]->fetch();
   }
 
 
   /**
    * Get stations for the specified highway
    *
-   * Detects all possible stations for a specific highway.
+   * Scopes stations and returns only the stations attached to the highwayid provided.
+   * This method does not limit the types of stations shown in any way.
    *
+   * @access public
    * @param int $id Highway ID
    * @return [Station]
    * @url GET {id}/stations
