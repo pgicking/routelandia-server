c488bc19239f2fb1b1511be9b7e6a6b94b10c3d7
Josh Proehl <josh@daedalusdreams.com>
2015-01-23 11:13:56 -0800

    Add buildBigLine() to create the whole highway JSON

--- entities/Highway.php
+++ entities/Highway.php
@@ -14,12 +14,32 @@ class Highway {
   public $direction;
   public $highwayname;
   public $bound;
+  public $fullGeoJson;
 
   // Hide some of the params that the client won't need.
   protected $highwaylength;
   protected $startmp;
   protected $endmp;
 
+
+  /**
+   * Gets all the stations for this highway, and concatenates their JSON segments together
+   * into a single giant JSON polyline, saving that into the $fullGeoJson field.
+   */
+  function buildBigLine() {
+    $output = new \stdClass();
+    $output->type = "Linestring";
+    $output->coordinates = array();
+
+    $ss = OrderedStation::fetchForHighway($this->highwayid);
+    foreach($ss as $ts) {
+      foreach($ts->geojson_raw->coordinates as $tc) {
+        $output->coordinates[] = $tc;
+      }
+    }
+
+    $this->fullGeoJson = $output;
+  }
 
   /******************************************************************************
    * STATIC CLASS METHODS
