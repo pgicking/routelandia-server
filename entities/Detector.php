80c7064e2bdbd25245c16a57c97d37947df1e5dc
Josh Proehl <josh@daedalusdreams.com>
2015-01-23 10:17:16 -0800

    Coding standards

--- entities/Detector.php
+++ entities/Detector.php
@@ -9,16 +9,17 @@ use Respect\Relational\Mapper;
  */
 class Detector{
 
-    /**
-     * Detectors have a "start_date" and "end_Date value". This function will
-     * determine if the detector is still active be looking if the end_date is null or not
-     *
-     * @return True or false
-     */
-    public function stillActive(){
-        if($this->end_date == null)
-            return true;
-        else
-            return false;
-    }
+  /**
+   * Detectors have a "start_date" and "end_Date value". This function will
+   * determine if the detector is still active be looking if the end_date is null or not
+   *
+   * @return True or false
+   */
+  public function stillActive(){
+      if($this->end_date == null)
+          return true;
+      else
+          return false;
+  }
+
 }
