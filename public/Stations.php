9025173e657d9e36ac65aaba4ba95bf7f7d0ee85
jessewagner <jesse.wagner00@gmail.com>
2015-02-01 21:28:18 -0800

    currently have the relatedOnRamps method renamed and have implemented a way to only return null in some fields for stations that do not exist, not sure if thats how we want to handle that case

--- public/Stations.php
+++ public/Stations.php
@@ -53,17 +53,23 @@ class Stations {
    * @param int $id The station ID to calculate related onramp ID for
    * @return stdClass $retVal An object containing the ID searched for, the calculated result, and an array of relation onramps.
    * @throws \Luracast\Restler\RestException
-   * @url GET {id}/relatedonramps
+   * @url GET {id}/relatedonramp
    */
-  public function getRelatedOnramps($id) {
-    $retVal = new stdClass;
+  public function getRelatedOnramp($id) {
+	
+	$retVal = new stdClass;
     $retVal->stationid = $id;
     $retVal->relatedOnrampId = Routelandia\Entities\Station::calculateRelatedOnrampID($id);
-    $retVal->relatedOnramps = array ();
-    $onRamp = OrderedStation::fetchRelatedOnramps($retVal->relatedOnrampId);
-    if ($onRamp){
-      $retVal->relatedOnramps[] = $onRamp;
+    if ($retVal->relatedOnrampId == null) {
+    	$onRamp = null;
+    } else {
+    	$retVal->relatedOnrampInfo = array ();
+    	$onRamp = OrderedStation::fetchRelatedOnramps($retVal->relatedOnrampId);
     }
+    if ($onRamp){
+    	$retVal->relatedOnrampInfo[] = $onRamp;
+    }
+    
     return $retVal;
   }
 
