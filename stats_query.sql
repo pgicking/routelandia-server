c4c7a52cb0763e3b64ca6c99c2d62e2b2c3cd324
Josh Proehl <josh@daedalusdreams.com>
2015-02-24 21:35:09 -0800

    Update demo query. Now shows correct aggregated results.
    Implements a "median" function to show final aggregate information as
    this seems to provide more realistic and useful data.

--- stats_query.sql
+++ stats_query.sql
@@ -6,6 +6,12 @@
 --    We rely on these to be formatted strings with the correct table names. Generation of this handled in application logic.
 --  * The start and end times we're interested in during those days, both in 24 hour '17:00' format.
 --    These times should be on 15 minute increments, as the query will be grouped into the 15 minute blocks. (:00, :15, :30, :45)
+SELECT extract('hour' from interval_start) as "hour",
+       extract('minute' from interval_start) as "min",
+       round2(median(avg_speed)) as "speed",
+       round2(median(avg_traveltime)) as "traveltime"
+FROM
+(
 SELECT min(interval_start) as "interval_start",
        round2(avg(avg_speed)) as "avg_speed",
        round2(sum(avg_traveltime)) as "avg_traveltime"
@@ -13,40 +19,54 @@ FROM
 (
 SELECT S.stationid,
        S.length,
-       -- extract('year' from starttime) as "year",
-       -- extract('month' from starttime) as "month",
-       -- extract('day' from starttime) as "day",
-       -- extract('hour' from starttime) as "hour",
-       -- min(extract('minute' from starttime)) as "min",
        min(starttime) as "interval_start",
        avg(D.speed) as "avg_speed",
        (S.length/avg(D.speed))*60 as "avg_traveltime"
       FROM
         (
-          -- for getting raw data we'll need to union two tables for EVERY DAY we want.
+          -- For getting raw data we'll need to union two tables for EVERY DAY we want, for every detector we want
+          -- However, for this example we're just getting the two, with just 4 detectors.
           SELECT detectorid,
                  starttime,
                  speed,
                  volume
-            FROM loopdata_2015_02_23
+            FROM loopdata_2015_02_19
             WHERE starttime::time >= '14:00'
-              AND starttime::time <= '16:00'
+              AND starttime::time <= '19:00'
               AND detectorid IN (100002, 100003, 100004, 100005)
           UNION
           SELECT detectorid,
                  starttime,
                  speed,
                  volume
-            FROM freeway.data_20150223
+            FROM freeway.data_20150219
             WHERE starttime::time >= '14:00'
-            AND starttime::time <= '16:00'
+            AND starttime::time <= '19:00'
+            AND detectorid IN (100002, 100003, 100004, 100005)
+          UNION
+          SELECT detectorid,
+                 starttime,
+                 speed,
+                 volume
+            FROM loopdata_2015_02_12
+            WHERE starttime::time >= '14:00'
+              AND starttime::time <= '19:00'
+              AND detectorid IN (100002, 100003, 100004, 100005)
+          UNION
+          SELECT detectorid,
+                 starttime,
+                 speed,
+                 volume
+            FROM freeway.data_20150212
+            WHERE starttime::time >= '14:00'
+            AND starttime::time <= '19:00'
             AND detectorid IN (100002, 100003, 100004, 100005)
           ORDER BY starttime,detectorid
         ) as D
         JOIN detectors dt ON d.detectorid = dt.detectorid
         JOIN stations S on dt.stationid = S.stationid
       WHERE starttime::time >= '14:00'
-        AND starttime::time <= '17:00'
+        AND starttime::time <= '19:00'
       GROUP BY S.stationid,
                extract('year' from starttime),
                extract('month' from starttime),
@@ -56,4 +76,7 @@ SELECT S.stationid,
       ORDER BY S.stationid, interval_start
 ) AS datatable
 GROUP BY interval_start
-ORDER BY interval_start;
+) AS outertable
+GROUP BY extract('hour' from interval_start),
+         extract('minute' from interval_start)
+ORDER BY hour,min;
