d3fae13af71ef15b8f14a5298df7b316a8681599
Josh Proehl <josh@daedalusdreams.com>
2015-02-27 23:12:29 -0800

    Update stats query to use 5min agg table

--- stats_query.sql
+++ stats_query.sql
@@ -2,14 +2,12 @@
 -- The detector list should be pre-built to be inserted into this query.
 -- In order to build it we need the following:
 --  * The list of detectors that we're interested in. !! NOTE: could make this a subquery rather than external?
---  * The table names for the days we want. (i.e. loopdata_yyyy_mm_dd and freeway.data_yyyymmdd for every day we're interested in)
---    We rely on these to be formatted strings with the correct table names. Generation of this handled in application logic.
 --  * The start and end times we're interested in during those days, both in 24 hour '17:00' format.
 --    These times should be on 15 minute increments, as the query will be grouped into the 15 minute blocks. (:00, :15, :30, :45)
 SELECT hour,
        minute,
        round2(median(avg_speed)) as "speed",
-       round(median(avg_traveltime)) as "traveltime"
+       round2(median(avg_traveltime)) as "traveltime"
   FROM
   (
     SELECT S.stationid,
@@ -21,58 +19,23 @@ SELECT hour,
            15*div(extract('minute' from starttime)::int, 15) as "minute",
            round2(avg(D.speed)) as "avg_speed",
            round2((S.length/avg(D.speed))*60) as "avg_traveltime"
-          FROM
-            (
-              -- For getting raw data we'll need to union two tables for EVERY DAY we want, for every detector we want
-              -- However, for this example we're just getting the two, with just 4 detectors.
-              SELECT detectorid,
-                     starttime,
-                     speed,
-                     volume
-                FROM loopdata_2015_02_19
-                WHERE starttime::time >= '14:00'
-                  AND starttime::time <= '19:00'
-                  AND detectorid IN (100002, 100003, 100004, 100005)
-              UNION ALL
-              SELECT detectorid,
-                     starttime,
-                     speed,
-                     volume
-                FROM freeway.data_20150219
-                WHERE starttime::time >= '14:00'
-                AND starttime::time <= '19:00'
-                AND detectorid IN (100002, 100003, 100004, 100005)
-              UNION ALL
-              SELECT detectorid,
-                     starttime,
-                     speed,
-                     volume
-                FROM loopdata_2015_02_12
-                WHERE starttime::time >= '14:00'
-                  AND starttime::time <= '19:00'
-                  AND detectorid IN (100002, 100003, 100004, 100005)
-              UNION ALL
-              SELECT detectorid,
-                     starttime,
-                     speed,
-                     volume
-                FROM freeway.data_20150212
-                WHERE starttime::time >= '14:00'
-                AND starttime::time <= '19:00'
-                AND detectorid IN (100002, 100003, 100004, 100005)
-              ORDER BY starttime,detectorid
-            ) as D
-            JOIN detectors dt ON d.detectorid = dt.detectorid
-            JOIN stations S on dt.stationid = S.stationid
-          WHERE starttime::time >= '14:00'
-            AND starttime::time <= '19:00'
-          GROUP BY S.stationid,
-                   extract('year' from starttime),
-                   extract('month' from starttime),
-                   extract('day' from starttime),
-                   extract('hour' from starttime),
-                   minute
-          ORDER BY S.stationid, year, month, day, hour, minute
-  ) AS agg_into_quarter_hour_buckets_for_each_day
+      FROM loopdata_5min_raw as D
+      JOIN detectors dt ON D.detectorid = dt.detectorid
+      JOIN stations S on dt.stationid = S.stationid
+      WHERE D.speed IS NOT NULL
+        AND D.speed != 0
+        AND D.detectorid IN (100002, 100003, 100004, 100005)
+        AND D.starttime >= (now() - '6 weeks'::interval)
+        AND extract('dow' from D.starttime) = 5
+        AND D.starttime::time >= '14:00'
+        AND D.starttime::time <= '18:00'
+      GROUP BY S.stationid,
+               extract('year' from D.starttime),
+               extract('month' from D.starttime),
+               extract('day' from D.starttime),
+               extract('hour' from D.starttime),
+               minute
+      ORDER BY S.stationid, year, month, day, hour, minute
+  ) AS fifteen_minute_agg
   GROUP BY hour, minute
   ORDER BY hour, minute;
