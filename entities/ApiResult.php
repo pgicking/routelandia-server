5638a3f024cd92d4a8be6ce78ee672224db0bc57
Josh Proehl <josh@daedalusdreams.com>
2015-03-07 13:36:45 -0800

    Tabs->spaces

--- entities/ApiResult.php
+++ entities/ApiResult.php
@@ -3,8 +3,8 @@
 namespace Routelandia\Entities;
 
 class ApiResult {
-	function __construct($input, $debug = null) {
-		is_array($input) ? $this->results = $input : $this->results = array($input);
-		if (!is_null($debug)) {$this->debug = $debug;}
-	}
+  function __construct($input, $debug = null) {
+    is_array($input) ? $this->results = $input : $this->results = array($input);
+    if (!is_null($debug)) {$this->debug = $debug;}
+  }
 }
