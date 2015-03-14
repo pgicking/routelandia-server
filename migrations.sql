ecc7b6d445a36172fcd92adb6f4b29c89c280444
Josh Proehl <josh@daedalusdreams.com>
2015-03-14 12:58:56 -0700

    OrderedStations reports stations without detectors
    Rather than strip out stations with no detectors and leave
    holes in the linked list, we're going to report the stations
    that have missing detectors, and let the accuracy metric
    used later on take the missing detectors into account.

--- migrations.sql
+++ migrations.sql
@@ -64,7 +64,7 @@ CREATE OR REPLACE VIEW routelandia.orderedStations AS WITH RECURSIVE stations_by
           AND s.end_date IS NULL        -- As long as it's actually a LIVE station
   )
 )
-SELECT * from stations_by_highway WHERE stationid IN (SELECT stationid FROM detectors WHERE enabledflag=1) ORDER BY linked_list_position;
+SELECT * from stations_by_highway ORDER BY linked_list_path
 
 
 -- VIEW: highwaysWithStations
